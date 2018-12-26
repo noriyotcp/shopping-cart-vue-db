88584b2fc27d2bcf6c45fdce03f233b65027446f
noriyotcp <noriyo.akita@gmail.com>
2018-12-26 20:41:15 +0900

    Add scripts and fix index.js

--- index.js
+++ index.js
@@ -1,39 +1,38 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000
 
 // Set default middlewares (logger, static, cors and no-cache)
-server.use(middlewares);
+server.use(middlewares)
 
 // Add custom routes before JSON Server router
 
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
-server.use(jsonServer.bodyParser);
+server.use(jsonServer.bodyParser)
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
+  )
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-Requested-With,content-type'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+  )
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  next()
+})
 server.use((req, res, next) => {
   if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
+    req.body.createdAt = Date.now()
   }
   // Continue to JSON Server router
-  next();
-});
+  next()
+})
 
 // Use default router
-server.use(router);
-server.listen(3000, () => {
-  console.log(`JSON Server is running at Port http://localhost:3000`); //eslint-disable-line no-console
-});
+server.use(router)
+server.listen(port)
